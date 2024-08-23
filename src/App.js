6945256ca6c93ef16b10998dcd590fd707849817
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-08-23 21:44:55 +0100

    Add files via upload
    Uploading source files.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./output.css";
+
+function App() {
+  return (
+    <div className="App w-100 h-dvh bg-orange-600 flex justify-center items-center">
+      <div className="card w-52 h-20 rounded-md bg-white flex justify-center items-center shadow-lg">
+        <h3 className=" text-blue-500 ">testing</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
